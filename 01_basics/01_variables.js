3ad2563bea9a28cffc6a3db30ed669257a31e4de
Nagaraj T Badiger <141129084+nagarajtb@users.noreply.github.com>
2024-12-24 05:23:36 +0000

    before data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = 'nagaraj@google.com'
+var accountPassword = "123456"
+accountCity = "Bangalore"
+
+// accountId = 122334
+accountEmail = "ntb@btn.com"
+accountPassword = "2122321"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity]);
