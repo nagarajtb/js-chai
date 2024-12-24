256168b710fd6b2f31c7f20e74a43c297c69a8a9
Nagaraj T Badiger <141129084+nagarajtb@users.noreply.github.com>
2024-12-24 05:49:13 +0000

    data types and ecma standards

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//alert('Hello', 3 + 3);
+
+let namee = "Naga" //string
+let age = 38 //number
+let isLoggedIn = false //boolean
+
+//null //standalone value which represents empty value
+//undefined //when value is not defined
+
+//symbol //unique
+//object
+//typeof // to know the data type
+
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
