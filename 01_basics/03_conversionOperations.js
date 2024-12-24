deef8f53ba7ff273f166cd00428f6b52ceb4b2e9
Nagaraj T Badiger <141129084+nagarajtb@users.noreply.github.com>
2024-12-24 06:29:42 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) //converting the value into number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "5234"
+let booleanIsLogIn = Boolean(isLoggedIn)
+console.log(booleanIsLogIn);
+console.log(typeof booleanIsLogIn);
