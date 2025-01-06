5f2e536b28fa020a2050d721a3d638444e1ce854
Nagaraj T Badiger <141129084+nagarajtb@users.noreply.github.com>
2025-01-06 05:45:50 +0000

    strings in detail

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name = "Nagaraj"
+const repoCount = 50
+
+//old style
+//console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('cobraJump');
+console.log(gameName[0]);
+console.log(gameName);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt('3'));
+console.log(gameName.indexOf('J'));
