5bbba13721ed9ea3e679bb8fdce01246c280e931
Nagaraj T Badiger <141129084+nagarajtb@users.noreply.github.com>
2025-01-27 08:43:01 +0000

    scopes

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -18,4 +18,54 @@ function addTwoNumbers(num1, num2) {
     
 }
 const noresult = addTwoNumbers(10, 50)
-console.log("Result:", noresult)
+//console.log("Result:", noresult)
+
+// function loginUserMessage(username) {
+//     if (username === undefined) {
+//         console.log("Please enter a user name");
+//         return
+//     } else {
+// return `${username} just logged in!`
+//     } 
+// }
+
+function loginUserMessage(username = "Sam") {
+    if (!username) {
+        console.log("Please enter a user name");
+        return
+    } else {
+        return `${username} just logged in!`
+    } 
+}
+
+//console.log(loginUserMessage("Tammanna"));
+
+function calculateCartPrice(...num1) {
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "Premnath",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`User name is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+//handleObject(user);
+
+handleObject({
+    username: "Nagaraj",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 500]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
---
+++ 03_basics/scope.js
@@ -0,0 +1,12 @@
+
+
+if (true) {
+    let abc = 10;
+    const bcd = 20;
+    var cde = 30;
+}  
+
+console.log(abc);
+console.log(bcd);
+console.log(cde);
+
