cc0837854c64a5f7f74468ddf4444b8d89a515d0
Nagaraj T Badiger <141129084+nagarajtb@users.noreply.github.com>
2025-01-27 05:47:27 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,21 @@
+function sayMyName() {
+    console.log("Nagaraj");
+    console.log('Tammanna');
+    console.log('Badiger');
+}
+
+//sayMyName();
+
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// addTwoNumbers(3225, null)
+
+function addTwoNumbers(num1, num2) {
+    //let result = num1 + num2 
+    return num1 + num2
+    
+}
+const noresult = addTwoNumbers(10, 50)
+console.log("Result:", noresult)
